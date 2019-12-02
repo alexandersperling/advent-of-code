66964f32f392cc0e232de4b69046598982e36fdd
alexandersperling <alexander@reinelt.de.com>
2019-12-02 10:57:50 +0100

    change from debug to normal log

---
+++ 2019/2/code.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const path = require('path')
+const readline = require('readline')
+
+const FILE_PATH = path.resolve('./modules')
+
+function calculateFuelAmount (mass) {
+  if (mass === 0) {
+    console.log(`Provided mass with value ${mass}, nothing to calculate`)
+    return 0
+  }
+  const fuelAmount = Math.floor((mass / 3)) - 2
+  console.debug(`Module with mass of ${mass} needs ${fuelAmount} amount of fuel`)
+  return fuelAmount
+}
+
+async function main () {
+  const rl = readline.createInterface({
+    input: fs.createReadStream(FILE_PATH)
+  })
+
+  let totalFuel = 0
+  let fuelBefore = 0
+  for await (const line of rl) {
+    let fuelAmount = calculateFuelAmount(line)
+    totalFuel += fuelAmount
+    fuelBefore = totalFuel - fuelAmount
+    console.loh(`Total amount of fuel increased from ${fuelBefore} to ${totalFuel}`)
+  }
+
+  console.log(`Total amount of fuel needed: ${totalFuel}`)
+}
+
+main()
